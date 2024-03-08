f44dbebb9f74be35fa5b44d77ff1730078adf9cb
Magnus Israel <magnus@mangusti.fi>
2024-03-08 23:27:01 +0200

    asdf

--- lua/nvim-terraform.lua
+++ lua/nvim-terraform.lua
@@ -1,9 +1,1 @@
-local M = {}
-
-function M.setup()
-	vim.api.nvim_create_user_command("SayHello", function()
-		print("Hello World")
-	end, { nargs = 0 })
-end
-
-return M
+print("nvin-terraform loaded")
