86460e1d86a3170577db3fb54f987d47709e50fa
Magnus Israel <magnus@mangusti.fi>
2024-03-08 23:21:31 +0200

    asdf

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,9 +1,10 @@
-local M = {}
-
-function M.setup()
-	vim.api.nvim_create_user_command("SayHello", function()
-		print("Hello World")
-	end, { nargs = 0 })
-end
-
-return M
+vim.keymap.set("n", "<leader>h", ":echo 'Hello World'<CR>", { noremap = true, silent = true })
+-- local M = {}
+--
+-- function M.setup()
+-- 	vim.api.nvim_create_user_command("SayHello", function()
+-- 		print("Hello World")
+-- 	end, { nargs = 0 })
+-- end
+--
+-- return M
